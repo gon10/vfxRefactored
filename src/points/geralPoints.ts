47350f8f27da6ece2a8aa8490a9a243a6b64c87d
Gonçalo <goncalo.pereira@Nrthree.com>
2022-04-22 21:32:58 +0100

    Change points to have the same name as the excel file

--- src/points/geralPoints.ts
+++ src/points/geralPoints.ts
@@ -9,230 +9,230 @@ let AllPoints: Point[];
 
 export default AllPoints = [
   {
-    name: "1-A bateria do tejo",
+    name: "Obra Militar N.º 1 - Bateria do Tejo",
     point: "1",
     position: {
       x: 30.23053074761363,
       y: -0.3,
-      z: 18.907219248411874
+      z: 18.907219248411874,
     },
     icon: "./img/pontoInteresse.jpg",
-    url: ""
+    url: "https://rhlt.cm-vfxira.pt/alhandra/om1",
   },
   {
-    name: "1-B Entrincheiramento",
+    name: "Obra Militar N.º 1-B - Entrincheiramento",
     point: "2",
     position: {
       x: 28.59148176356971,
       y: -0.4,
-      z: 18.02322221537706
+      z: 18.02322221537706,
     },
 
     icon: "./img/circuito.png",
-    url: "https://rhlt.cm-vfxira.pt/vfx/varina"
+    url: "https://rhlt.cm-vfxira.pt/alhandra/om1b",
   },
   {
-    name: "1-C Bateria da estrada",
+    name: "Obra Militar N.º 1-C - Bateria da Estrada",
     point: "3",
     position: {
       x: 26.761283898373755,
       y: -0.4,
-      z: 18.159653920798277
+      z: 18.159653920798277,
     },
 
     icon: "./img/pontoInteresse.jpg",
-    url: ""
+    url: "https://rhlt.cm-vfxira.pt/alhandra/om1c",
   },
   {
-    name: "1-D Bateria da estrada",
+    name: "Obra Militar N.º 1-D - Bateria da Estrada",
     point: "4",
     position: {
       x: 26.8544306961076,
       y: -0.3,
-      z: 17.790129656159756
+      z: 17.790129656159756,
     },
 
     icon: "./img/pontoInteresse.jpg",
-    url: ""
+    url: "https://rhlt.cm-vfxira.pt/alhandra/om1d",
   },
   {
-    name: "1-E Bateria da estrada real",
+    name: "Obra Militar N.º 1-E - Bateria da Estrada Real",
     point: "5",
     position: {
       x: 26.518229008302768,
       y: -0.2,
-      z: 17.118914271095626
+      z: 17.118914271095626,
     },
 
     icon: "./img/pontoInteresse.jpg",
-    url: ""
+    url: "https://rhlt.cm-vfxira.pt/alhandra/om1e",
   },
   {
-    name: "Monumento",
+    name: "Monumento Comemorativo das Linhas de Torres Vedras",
     point: "5",
     position: { x: 23.57153364592216, y: 0.4, z: 16.717983993564314 },
 
     icon: "./img/pontoInteresse.jpg",
-    url: ""
+    url: "https://rhlt.cm-vfxira.pt/alhandra/monumento/",
   },
   {
-    name: "1-F Bateria da subida",
+    name: "Obra Militar N.º 1-F - Bateria da Subida",
     point: "6",
     position: {
       x: 24.216891565755297,
       y: 0.4,
-      z: 17.09749272522543
+      z: 17.09749272522543,
     },
 
     icon: "./img/pontoInteresse.jpg",
-    url: ""
+    url: "https://rhlt.cm-vfxira.pt/alhandra/om1f",
   },
   {
-    name: "2 Bateria do Conde",
+    name: "Obra Militar N.º 2 - Bateria do Conde",
     point: "7",
     position: {
       x: 23.1736427868142,
       y: 0.4,
-      z: 16.318345727524974
+      z: 16.318345727524974,
     },
 
     icon: "./img/pontoInteresse.jpg",
-    url: "https://exposicoes.cm-vfxira.pt/oculistanunes/"
+    url: "https://rhlt.cm-vfxira.pt/alhandra/om2",
   },
   {
-    name: "3 Reduto da Boavista",
+    name: "Obra Militar nº 3 - Reduto da Boa Vista",
     point: "8",
     position: {
       x: 22.547911521485112,
       y: 0.4,
-      z: 16.56152279542322
+      z: 16.56152279542322,
     },
 
     icon: "./img/pontoInteresse.jpg",
-    url: ""
+    url: "https://rhlt.cm-vfxira.pt/alhandra/om3",
   },
   {
-    name: "4 Bateria do S. Fernando",
+    name: "Obra Militar N.º 4 - Reduto de São Fernando",
     point: "9",
     position: {
       x: 22.198731060284317,
       y: 0.5,
-      z: 16.625788592973446
+      z: 16.625788592973446,
     },
 
     icon: "./img/pontoInteresse.jpg",
-    url: ""
+    url: "https://rhlt.cm-vfxira.pt/alhandra/om4",
   },
   {
-    name: "114 Forte 1º de Subserra",
+    name: "Obra Militar N.º 114 - Forte 1º da Subserra",
     point: "10",
     position: { x: 17.021444903472425, y: 0.8, z: 16.422024812458158 },
 
     icon: "./img/pontoInteresse.jpg",
-    url: ""
+    url: "https://rhlt.cm-vfxira.pt/sjmontes/om114a",
   },
   {
-    name: "114-A Bateria Nova de Subserra",
+    name: "Obra Militar N.º 114A - Bateria Nova de Subserra",
     point: "11",
     position: {
       x: 14.006451087141238,
       y: 1.1120174662857135,
-      z: 15.540322124911407
+      z: 15.540322124911407,
     },
 
     icon: "./img/pontoInteresse.jpg",
-    url: ""
+    url: "https://rhlt.cm-vfxira.pt/sjmontes/om114a",
   },
   {
-    name: "115 Forte 2º de Subserra",
+    name: "Obra Militar N.º 115 - Reduto 2º de Subserra",
     point: "12",
     position: {
       x: 4.031474474898191,
       y: 1.9,
-      z: 9.362788038782961
+      z: 9.362788038782961,
     },
 
     icon: "./img/pontoInteresse.jpg",
-    url: "https://rhlt.cm-vfxira.pt/vfx/mario-coelho"
+    url: "https://rhlt.cm-vfxira.pt/sobralinho/om115",
   },
   {
-    name: "116 Forte 3º de Subserra",
+    name: "Obra Militar N.º 116 - Reduto do Casal da Entrega  ou Forte 3º da Subserra",
     point: "13",
     position: { x: 1.7729593588224801, y: 2.1, z: 7.116445396500171 },
 
     icon: "./imgpontoInteresse.jpg",
-    url: "https://exposicoes.cm-vfxira.pt/casamuseumariocoelho"
+    url: "https://rhlt.cm-vfxira.pt/alverca/om116",
   },
   {
-    name: "116-A Bateria Anexa ao Forte 3º de Subserra",
+    name: "Obra Militar N.º 116-A - Bateria Anexa ao Forte 3 da Subserra",
     point: "14",
     position: { x: 2.398053161855468, y: 1.8, z: 6.523440874041707 },
 
     icon: "./img/pontoInteresse.jpg",
-    url: ""
+    url: "https://rhlt.cm-vfxira.pt/sobralinho/om116a",
   },
   {
-    name: "116-B Bateria do Casal da Entrega",
+    name: "Obra Militar N.º 116-B - Bateria do Casal da Entrega",
     point: "14",
     position: { x: 2.486458492334099, y: 1.7, z: 5.3366029659148175 },
 
     icon: "./img/pontoInteresse.jpg",
-    url: ""
+    url: "https://rhlt.cm-vfxira.pt/sjmontes/om116b",
   },
   {
-    name: "117 Reduto Novo da Costa da Feira",
+    name: "Obra Militar N.º 117 - Reduto Novo da Costa da Freira",
     point: "14",
     position: { x: -0.6109221435383516, y: 2.3, z: 3.3659116713200437 },
 
     icon: "./img/pontoInteresse.jpg",
-    url: ""
+    url: "https://rhlt.cm-vfxira.pt/calhandriz/om117",
   },
   {
-    name: "118 Forte dos Sinais (Forte do Moinho Branco)",
+    name: "Obra Militar N.º 118 - Forte do Moinho Branco ou dos Sinais",
     point: "14",
     position: { x: -2.373377076635828, y: 2.4, z: 5.351952588606054 },
 
     icon: "./img/pontoInteresse.jpg",
-    url: ""
+    url: "https://rhlt.cm-vfxira.pt/alverca/om118",
   },
   {
-    name: "120 Forte da Serra do Formoso",
+    name: "Obra Militar N.º 120 - Forte Novo da Serra do Formoso",
     point: "14",
     position: { x: -6.730727773175402, y: 2.4, z: -0.5198298203711764 },
 
     icon: "./img/pontoInteresse.jpg",
-    url: ""
+    url: "https://rhlt.cm-vfxira.pt/calhandriz/om120",
   },
   {
-    name: "5 Forte Novo do Formoso",
+    name: "Obra Militar N.º 5 - Reduto da Serra do Formoso",
     point: "14",
     position: { x: -7.5556202770984155, y: 2.4, z: -0.7422221493926707 },
 
     icon: "./img/pontoInteresse.jpg",
-    url: ""
+    url: "https://rhlt.cm-vfxira.pt/calhandriz/om5",
   },
   {
-    name: "119 Reduto dos Dois Moinhos",
+    name: "Obra Militar N.º 119 -Forte dos Dois Moinhos de Sarnadas",
     point: "14",
     position: { x: -8.84753939561242, y: 2.1, z: 8.532407786151802 },
 
     icon: "./img/pontoInteresse.jpg",
-    url: ""
+    url: "https://rhlt.cm-vfxira.pt/alverca/om119",
   },
   {
-    name: "119-B Bateria da Subida da Serra",
+    name: "Obra Militar Nº119-b Bateria da Subida da Serra",
     point: "14",
     position: { x: -13.246875442489198, y: 1.1, z: 10.409754978023498 },
 
     icon: "./img/pontoInteresse.jpg",
-    url: ""
+    url: "https://rhlt.cm-vfxira.pt/calhandriz/om119b",
   },
   {
-    name: "6 Bateria dos Melros",
+    name: "Obra Militar N.º 6 - Bateria dos Melros",
     point: "14",
     position: { x: -12.657366631424296, y: 0.9, z: 14.501919086605678 },
 
     icon: "./img/pontoInteresse.jpg",
-    url: ""
-  }
+    url: "https://rhlt.cm-vfxira.pt/alverca/om6",
+  },
 ];
