acd0a1932f0d19a9ce9bd2b4ce404582968bbc72
Gonçalo <goncalo.pereira@Nrthree.com>
2022-09-25 15:58:02 +0100

    Add points

--- src/points/geralPoints.ts
+++ src/points/geralPoints.ts
@@ -9,158 +9,36 @@ let AllPoints: Point[];
 
 export default AllPoints = [
   {
-    name: "Busto do José Júlio",
-    point: "1",
+    name: "	Obra Militar nº 40, Forte da Aguieira/Casa da Portela	",
+    point: "	1	",
     position: {
-      x: -0.6446135139415828,
-      y: -0.5,
-      z: -0.7076798387903979
+      x: 2.0762296174538615,
+      y: 0.7917662200703572,
+      z: 1.2646674766886876,
     },
-    icon: "./img/pontoInteresse.jpg",
-    url: ""
+    icon: "./img/	pontoInteresse.jpg",
+    url: "https://rhlt.cm-vfxira.pt/vialonga/om40",
   },
   {
-    name: "Monumento à Varina",
-    point: "2",
+    name: "	Obra Militar N.º 41 - Forte da Portela Pequena	",
+    point: "	2	",
     position: {
-      x: -0.35375702368778833,
-      y: -0.5,
-      z: -1.4082805597608636
+      x: 1.7657925361059656,
+      y: 0.8100424344526835,
+      z: 2.744360441557006,
     },
-
-    icon: "./img/circuito.png",
-    url: "https://rhlt.cm-vfxira.pt/vfx/varina"
+    icon: "./img/	pontoInteresse.jpg",
+    url: "	https://rhlt.cm-vfxira.pt/vialonga/om41",
   },
   {
-    name: "Celeiro da Patriarcal",
-    point: "3",
+    name: "	Obra Militar nº nº 42, Forte da Portela Grande	",
+    point: "	3	",
     position: {
-      x: 0.30032433580827633,
-      y: -0.5,
-      z: -0.4905834495185127
+      x: 2.8239297697628136,
+      y: 0.67743163054459705,
+      z: 3.9548638287576825,
     },
-
-    icon: "./img/pontoInteresse.jpg",
-    url: ""
+    icon: "./img/	pontoInteresse.jpg",
+    url: "https://rhlt.cm-vfxira.pt/vialonga/om42",
   },
-  {
-    name: "Monumento ao Campino",
-    point: "4",
-    position: {
-      x: 0.0751304197038371,
-      y: -0.5,
-      z: -1.0400839921457148
-    },
-
-    icon: "./img/pontoInteresse.jpg",
-    url: ""
-  },
-  {
-    name: "Paços do Concelho",
-    point: "5",
-    position: {
-      x: -0.16547757633544058,
-      y: -0.5,
-      z: -1.506937327180205
-    },
-
-    icon: "./img/pontoInteresse.jpg",
-    url: ""
-  },
-  {
-    name: "Pelourinho Manuelinho De Vila Franca de Xira",
-    point: "6",
-    position: { x: -0.05418853155022726, y: -0.5, z: -1.7441388111330685 },
-
-    icon: "./img/pontoInteresse.jpg",
-    url: ""
-  },
-  {
-    name: "Oculista Nunes",
-    point: "7",
-    position: { x: -0.022537940505767528, y: -0.5, z: -1.8532822917562257 },
-
-    icon: "./img/pontoInteresse.jpg",
-    url: "https://exposicoes.cm-vfxira.pt/oculistanunes/"
-  },
-  {
-    name: "Antigo Edifício da Biblioteca Museu Dr. Vidal Baptista",
-    point: "8",
-    position: { x: 0.10928876432273713, y: -0.5, z: -1.8424108353450865 },
-
-    icon: "./img/pontoInteresse.jpg",
-    url: ""
-  },
-  {
-    name: "Café Central / Posto do Turismo",
-    point: "9",
-    position: {
-      x: 0.2748419633490707,
-      y: -0.5,
-      z: -2.0488332625786603
-    },
-
-    icon: "./img/pontoInteresse.jpg",
-    url: ""
-  },
-  {
-    name: "Museu Municipal de Vila Franca de Xira",
-    point: "10",
-    position: {
-      x: 1.7619046779490242,
-      y: -0.5,
-      z: -1.2920911336404695
-    },
-
-    icon: "./img/pontoInteresse.jpg",
-    url: ""
-  },
-  {
-    name: "Museu do Neorrealismo",
-    point: "11",
-    position: {
-      x: 0.6631364873755742,
-      y: -0.5,
-      z: -2.864782462513777
-    },
-
-    icon: "./img/pontoInteresse.jpg",
-    url: ""
-  },
-  {
-    name: "Busto de Mário Coelho",
-    point: "12",
-    position: {
-      x: -0.061285959430493905,
-      y: -0.5,
-      z: -4.140694791662458
-    },
-
-    icon: "./img/pontoInteresse.jpg",
-    url: "https://rhlt.cm-vfxira.pt/vfx/mario-coelho"
-  },
-  {
-    name: "Casa Museu Mário Coelho",
-    point: "13",
-    position: {
-      x: 0.32572574476821337,
-      y: -0.5,
-      z: -4.336233316569454
-    },
-
-    icon: "./imgpontoInteresse.jpg",
-    url: "https://exposicoes.cm-vfxira.pt/casamuseumariocoelho"
-  },
-  {
-    name: "Igreja do Mártir Santo",
-    point: "14",
-    position: {
-      x: 1.3907003922051622,
-      y: -0.5,
-      z: -5.643588536887085
-    },
-
-    icon: "./img/pontoInteresse.jpg",
-    url: ""
-  }
 ];
